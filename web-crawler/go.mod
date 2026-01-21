06ebac22e727dc8d57dbe4dfcc4ec6278b9a6439
thenatespack <natespack@icloud.com>
2026-01-20 23:08:16 -0700

    get that go out of here

--- web-crawler/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/timmyjinks/web-crawler
-
-go 1.25.4
-
-require golang.org/x/net v0.48.0
--- web-crawler/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-func main() {
-	url := "http://localhost:8080"
-	crawler := NewSkibidiCrawler(url)
-	crawler.Crawl()
-}
